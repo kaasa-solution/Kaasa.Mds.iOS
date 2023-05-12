9bfe6b029cbe4a6cc90e184de2033bad5ce2a506
Vincent Uhlmann <contact@vuhlmann.com>
2023-05-12 22:42:40 +0200

    Updated csproj and build script

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ XCODE_BUILD=/Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild
 SHARPIE_OUTPUT_FOLDER=$PROJECT_NAME/SharpieGenerated
 MDS_PROJECT=movesense-mobile-lib/IOS/MovesenseApi/MovesenseApi.xcodeproj
 MDS_FRAMEWORK=movesense-mobile-lib/IOS/MovesenseApi/build/Release-iphoneos/MovesenseApi.framework
+IPHONE_SDK=$(sharpie xcode -sdks | grep -wo "iphoneos[0-9.]*")
 
 rm -rf $PROJECT_NAME/bin
 rm -rf $PROJECT_NAME/obj
@@ -16,6 +17,6 @@ mkdir -p $BUILD_FOLDER
 
 $XCODE_BUILD ONLY_ACTIVE_ARCH=NO ENABLE_BITCODE=NO ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES=NO -project $MDS_PROJECT -configuration Release -sdk iphoneos build
 
-sharpie bind -sdk iphoneos -o $SHARPIE_OUTPUT_FOLDER -n $PROJECT_NAME -f $MDS_FRAMEWORK
+sharpie bind -sdk $IPHONE_SDK -o $SHARPIE_OUTPUT_FOLDER -n $PROJECT_NAME -f $MDS_FRAMEWORK
 
 dotnet build $PROJECT_NAME/$PROJECT -c Release
